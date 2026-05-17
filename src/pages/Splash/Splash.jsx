f2dc614b7c6ee7fa37613eb62746e6bd333e394a
huieunkim-dev <huieunkim.dev@gmail.com>
2026-05-17 17:03:08 +0900

    feat: add Welcome page

--- src/pages/Splash/Splash.jsx
+++ src/pages/Splash/Splash.jsx
@@ -8,7 +8,7 @@ export default function Splash() {
 
   const handleStart = () => {
     resetAnswers(); // 이전 선택값 초기화
-    navigate("/step1"); // Step1으로 이동
+    navigate("/welcome");
   };
 
   return (
--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,5 +1,6 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 import Splash from "../pages/Splash/Splash";
+import Welcome from "../pages/Welcome/Welcome";
 import Step1Budget from "../pages/Step1Budget/Step1Budget";
 import Step2Vibe from "../pages/Step2Vibe/Step2Vibe";
 import Step3Menu from "../pages/Step3Menu/Step3Menu";
@@ -14,6 +15,7 @@ export default function AppRouter() {
   return (
     <Routes>
       <Route path="/" element={<Splash />} />
+      <Route path="/welcome" element={<Welcome />} />
       <Route path="/step1" element={<Step1Budget />} />
       <Route path="/step2" element={<Step2Vibe />} />
       <Route path="/step3" element={<Step3Menu />} />
@@ -22,7 +24,7 @@ export default function AppRouter() {
       <Route path="/result" element={<SelectCardPage />} />
       <Route path="/directions" element={<DirectionsView />} />
       <Route path="/confirm" element={<ConfirmComplete />} />
-      <Route path="/detail" element={<DetailOverlay />}/>
+      <Route path="/detail" element={<DetailOverlay />} />
       <Route path="*" element={<Navigate to="/" replace />} />
     </Routes>
   );
