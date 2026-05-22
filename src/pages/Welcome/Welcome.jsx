2bb1ad656554b598756c0993c7587ddc70500264
huieunkim-dev <huieunkim.dev@gmail.com>
2026-05-22 17:01:12 +0900

    refactor: refactor styles

--- src/pages/Welcome/Welcome.jsx
+++ src/pages/Welcome/Welcome.jsx
@@ -1,6 +1,5 @@
 import { useNavigate } from "react-router-dom";
 import styles from "./Welcome.module.scss";
-import WelcomeTop from "../../assets/welcome-top.svg";
 import WelcomeImg from "../../assets/welcome-img.svg";
 
 export default function Welcome() {
@@ -8,13 +7,15 @@ export default function Welcome() {
 
   return (
     <div className={styles.container}>
-      <img src={WelcomeTop} alt="반가워요 00님!" className={styles.topText} />
-      <img src={WelcomeImg} alt="welcome" className={styles.mainImg} />
-      <p className={styles.desc}>
-        초이스 잇이 ㅇㅇ님의 먹거리 고민을 위해
-        <br />
-        몇가지 질문할게요!
-      </p>
+      <p className={styles.topText}>반가워요 00님!</p>
+      <div className={styles.contentArea}>
+        <img src={WelcomeImg} alt="welcome" className={styles.mainImg} />
+        <p className={styles.desc}>
+          초이스 잇이 ㅇㅇ님의 먹거리 고민을 위해
+          <br />
+          몇가지 질문할게요!
+        </p>
+      </div>
       <button className={styles.btn} onClick={() => navigate("/step1")}>
         질문하러 가기
       </button>
