55951640f5404dacbb354f515c9d659406203f55
huieunkim-dev <huieunkim.dev@gmail.com>
2026-05-28 21:25:44 +0900

    fix: rewrite 제거

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,6 @@ export default defineConfig({
       '/api': {
         target: 'http://13.125.137.196:8080',
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
       },
     },
   },
